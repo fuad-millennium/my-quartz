bb36dba50b8344dbb2763099ac615d58070d97fd
Fuad Hasan <fuad.hasan@mislbd.com>
2025-07-09 13:58:31 +0600

    config file is changed

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://fuad-millennium.github.io/my-quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
